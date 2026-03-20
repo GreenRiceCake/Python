5084f923d135a3c365d4b978de2e18eed79eb89d
GreenRC <78803197+GreenRiceCake@users.noreply.github.com>
2026-03-20 11:52:12 +0900

    Update fact.py

--- 0320/fact.py
+++ 0320/fact.py
@@ -2,7 +2,12 @@ num = int(input("Enter a number: "))
 
 fact = 1
 
-for i in range(1, num + 1):
+print(num, end="! = ")
+for i in range(num, 0, -1):
     fact *= i
+    if i > 1:
+        print(i, end=" * ")
+    else :
+        print(i, end=" = ")
 
-print("Factorial of", num, "is", fact)
+print(fact)
